17ce8734d401e9fc4fda622d241cd0965e43d7a5
Arthur R Longbottom <art.longbottom.jr@gmail.com>
2025-09-10 13:18:35 -0700

    Integrate Leva controls into Header component for enhanced UI customization. Implement state management for collapsed view and loading effects. Refactor Middle component by removing unused Loader import to streamline dependencies.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import { useSnapshot } from "valtio";
 import { filterStore } from "../store/filterStore";
 import { animated, useSpring } from "@react-spring/web";
+import { Leva } from "leva";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 export const Header = () => {
   const { search } = useSnapshot(filterStore);
@@ -11,6 +14,16 @@ export const Header = () => {
     borderColor: !isSearching ? "#00000000" : "#dadada66",
     backgroundColor: !isSearching ? "#fffee922" : "#00000000",
   });
+
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  const [levaLoaded, setLevaLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLevaLoaded(true);
+    }, 1000);
+  }, []);
+
   return (
     <animated.div
       style={spring}
@@ -29,6 +42,21 @@ export const Header = () => {
           <Link href="/podcast">Podcast</Link>
           <button>Menu</button>
         </div>
+      </div>
+      <div
+        className={cn(
+          "absolute top-0 right-0 opacity-0 hover:opacity-100 transition-opacity duration-300",
+          levaLoaded
+            ? isCollapsed
+              ? "opacity-30"
+              : "opacity-100"
+            : "opacity-0"
+        )}
+      >
+        <Leva
+          collapsed={{ collapsed: isCollapsed, onChange: setIsCollapsed }}
+          isRoot
+        />
       </div>
     </animated.div>
   );
--- app/components/Middle.tsx
+++ app/components/Middle.tsx
@@ -1,4 +1,4 @@
-import { Loader, ScrollControls } from "@react-three/drei";
+import { ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import App from "./App";
 import * as THREE from "three";
