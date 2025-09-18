3987aecee3b6c3c3f4704285d90e4b51cb4d917e
Arthur R Longbottom <art.longbottom.jr@gmail.com>
2025-09-18 09:26:03 -0700

    Update Skybox component to enhance ground controls and texture handling. Adjust groundHeight, groundRadius, groundScale, and groundRotation values for improved scene configuration. Introduce texture control for dynamic environment mapping and refactor skybox positioning and scaling logic.

--- app/components/Skybox.tsx
+++ app/components/Skybox.tsx
@@ -1,52 +1,57 @@
 import { useLoader } from "@react-three/fiber";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { UltraHDRLoader } from "three/examples/jsm/Addons.js";
-import { EquirectangularReflectionMapping } from "three";
+import { EquirectangularReflectionMapping, Vector3 } from "three";
 import { GroundedSkybox } from "three/examples/jsm/Addons.js";
 import { useControls } from "leva";
 import { Select } from "@react-three/postprocessing";
 
 export default function Skybox() {
-  const { groundHeight, groundRadius, groundScale, groundRotation } =
+  const { groundHeight, groundRadius, groundScale, groundRotation, texture } =
     useControls("Environment Ground", {
       groundHeight: {
-        value: 0.95,
-        min: 0,
+        value: 4,
+        min: 0.01,
         max: 10,
         step: 0.01,
         label: "Ground Height",
       },
       groundRadius: {
-        value: 2,
+        value: 10.8,
         min: 0,
         max: 50,
         step: 0.01,
         label: "Ground Radius",
       },
       groundScale: {
-        value: 1,
+        value: 1.02,
         min: 0,
         max: 5,
         step: 0.01,
         label: "Ground Scale",
       },
       groundRotation: {
-        value: [0.0, 1.35, 0],
-        step: 0.01,
-        label: "Ground Rotation",
+        value: { x: 0.0, y: 1.2, z: 0 },
+      },
+      // https://gainmap-creator.monogrid.com/ to convert .hdr to .jpg
+      texture: {
+        image: "/painted-dog-scene_5.jpg",
       },
     });
 
-  const envMap = useLoader(UltraHDRLoader, "/painted-dog-scene_5.jpg");
+  const envMap = useLoader(
+    UltraHDRLoader,
+    texture || "/painted-dog-scene_5.jpg"
+  );
 
   const skybox = useMemo(() => {
     if (!envMap) return null;
     envMap.mapping = EquirectangularReflectionMapping;
     const skybox = new GroundedSkybox(envMap, groundHeight, groundRadius, 2048);
-    skybox.position.y = groundHeight - 0.5;
-    skybox.scale.setScalar(2);
+    // skybox.position.y = groundHeight - 0.5;
+    // skybox.scale.setScalar(2);
     return skybox;
-  }, [envMap, groundRadius, groundHeight]);
+  }, [envMap, groundRadius, groundHeight, texture]);
 
   return (
     <Select enabled={false}>
@@ -55,7 +60,11 @@ export default function Skybox() {
           object={skybox}
           position={[0, groundHeight, 0]}
           scale={groundScale}
-          rotation={groundRotation}
+          rotation={new Vector3(
+            groundRotation.x,
+            groundRotation.y,
+            groundRotation.z
+          ).toArray()}
         />
       )}
     </Select>
