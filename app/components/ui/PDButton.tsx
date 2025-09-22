d9e15039f87df1997a9a5c75c71908c1c102d741
Arthur R Longbottom <art.longbottom.jr@gmail.com>
2025-09-22 10:47:32 -0700

    Enhance PDButton component styling with improved hover and active states. Refactor class names for better transition effects and responsiveness.

--- app/components/ui/PDButton.tsx
+++ app/components/ui/PDButton.tsx
@@ -13,8 +13,11 @@ export const PDButton = ({
   return (
     <button
       className={cn(
-        "bg-[#F9F6F0] text-black px-3 h-8 rounded-sm border border-black font-medium cursor-pointer whitespace-nowrap",
-        primary && "bg-black text-white",
+        "px-3 h-8 rounded-sm border border-black font-medium cursor-pointer whitespace-nowrap transition-all duration-100",
+        "hover:translate-y-[-2px] hover:shadow-md",
+        primary
+          ? "bg-black hiov text-white active:outline active:outline-black"
+          : "bg-[#F9F6F0] hover:bg-[#F2EFE9] active:bg-[#EBE6DB] text-black",
         className
       )}
       {...props}
