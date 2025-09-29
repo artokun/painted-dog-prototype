a9eea96c31dcfb919240b237c3f7d74515e9d786
Arthur R Longbottom <art.longbottom.jr@gmail.com>
2025-09-29 09:31:51 -0700

    Update NotFoundContent component for improved styling and layout
    - Increased the font size of the 404 header for better visibility.
    - Adjusted margin for the video container and button to enhance spacing and overall layout.
    - Ensured a more user-friendly presentation of the not found message.

--- app/components/NotFoundContent.tsx
+++ app/components/NotFoundContent.tsx
@@ -13,8 +13,8 @@ export const NotFoundContent = ({ visible }: { visible: boolean }) => {
       className="absolute inset-0 h-dvh w-dvw text-black z-10 overflow-y-auto pointer-events-auto overflow-x-hidden flex flex-col"
     >
       <div className="flex flex-col flex-1 gap-5 items-center justify-center max-w-lg text-center mx-auto w-full mt-20 px-2 pt-20">
-        <h1 className="text-4xl font-medium">404</h1>
-        <div className="w-80 h-80 my-4">
+        <h1 className="text-5xl font-medium">404</h1>
+        <div className="w-80 h-80 mt-10">
           <video
             src="/dog-loop.mp4"
             autoPlay
@@ -27,7 +27,7 @@ export const NotFoundContent = ({ visible }: { visible: boolean }) => {
         <p className="text-lg">
           Not to worry though, you can continue to browse from the homepage.
         </p>
-        <PDButton href="/" className="w-full max-w-[250px]" primary tall>
+        <PDButton href="/" className="w-full max-w-[250px] mt-8" primary tall>
           Back to home
         </PDButton>
       </div>
