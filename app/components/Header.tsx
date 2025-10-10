6dd63ee1e02b53de066651d811dd6c6672a93667
Arthur R Longbottom <art.longbottom.jr@gmail.com>
2025-10-10 08:11:46 -0700

    Update Header component to change visibility class from 'visible' to 'hidden' for improved UI management

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -142,7 +142,7 @@ export const Header = () => {
           </div>
         </div>
       </div>
-      <div className="visible">
+      <div className="hidden">
         <Leva
           collapsed={{ collapsed: isCollapsed, onChange: setIsCollapsed }}
           isRoot
