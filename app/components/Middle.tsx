413396eaaeae5e185a1a119594bc388337752412
Abram Manaka <abram@plusnarrative.com>
2026-02-26 03:53:48 +0200

    fix: reveal on scroll and missing overlay underlines

--- app/components/Middle.tsx
+++ app/components/Middle.tsx
@@ -158,7 +158,7 @@ const TempAcceleratedContent = forwardRef<HTMLDivElement, {}>((_, ref) => {
           <PurchaseButtons layout="horizontal" isMobile={isMobile} />
         </div>
         <div className="flex-1 gap-4 flex flex-col">
-          <h3 className="text-xl font-medium">Upcoming Events</h3>
+          {/* <h3 className="text-xl font-medium">Upcoming Events</h3> */}
           <p className="text-md">
             <i>Bitterkomix: Sketchbooks and Journals</i> will be launched at the
             Book Lounge, Cape Town, on 5 December 2025. Please arrive 17:30 for
@@ -182,22 +182,18 @@ const TempAcceleratedContent = forwardRef<HTMLDivElement, {}>((_, ref) => {
         </div>
       </div>
       <div className="border-b border-black w-[calc(100dvw-64px)] max-w-[400px] mx-auto h-px pt-10 md:pt-3 mb-10 md:mb-0" />
-      <div className="flex flex-col md:flex-row gap-20 md:gap-10 max-w-3xl mx-auto px-8 md:px-0">
-        <article className="flex-1 flex flex-col gap-4">
-          <h3 className="text-xl font-medium">New Publisher, New Tricks</h3>
+      <div className="flex flex-co  md:flex-row gap-20 md:gap-10 max-w-[540px] mx-auto px-8 md:px-0">
+        <article className="flex-1 flex flex-col gap-4 text-center">
+          <h3 className="text-[32px] font-semibold">So, what's next ?</h3>
           <p>
-            Painted Dog Press is a new independent book publisher of fiction and
-            non-fiction. Spearheaded by Fourie Botha (previously from Penguin
-            Random House SA) and John Hunt (TBWA/Hunt/Lascaris), the press will
-            develop and nurture quality literature and provide writers with a
-            publishing house that continually fosters and markets their work.
-            Painted Dog&apos;s efforts will be strengthened by tech innovation
-            and human-first technology.
+            We have many a literary treat for book lovers of all kinds in the
+            works. Join our newsletter community to be the first to get
+            exclusive author interviews, book snippets and insider treats.
           </p>
         </article>
-        <article className="flex-1 flex flex-col gap-4">
+        {/* <article className="flex-1 flex flex-col gap-4">
           <NewsletterForm />
-        </article>
+        </article> */}
       </div>
       <Footer />
     </animated.section>
